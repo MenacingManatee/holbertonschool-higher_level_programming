b2dcc7c4c07c5b4f4dc3999f6902c3ce1bd7e4f2
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-28 21:29:33 +0000

    Each step loops through previous list nodes to see if list has looped

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,12 +1,25 @@
-#include <stdio.h>
+#include "lists.h"
 
 /**
- * main - 
+ * check_cycle - checks if a linked list has a loop
+ * @list: linked list to check
  *
- * Return: Always 0 (ok)
+ * Return: 0 if there is no cycle, 1 if there is a cycle
  */
-int main(void)
+int check_cycle(listint_t *list)
 {
-	
+	listint_t *current, *check;
+	int i, j;
+
+	check = current = list;
+	for (i = 0; current; i++)
+	{
+		check = list;
+		current = current->next;
+		for (j = 0; check != current && j < i; j++)
+			check = check->next;
+		if (check == current)
+			return (1);
+	}
 	return (0);
 }
