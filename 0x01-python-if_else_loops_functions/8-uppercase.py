11b147669f7a41760245f5c171b4fed246b81fa2
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-28 09:57:53 +0000

    Probably overcomplicated this one. Casts to list at the start, then checks each item and replaces with the uppercase value as needed

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,2 +1,13 @@
 #!/usr/bin/python3
-
+def islower(a):
+    a = ord(a)
+    if a > 96:
+        return(a - (ord("a") - ord("A")))
+    else:
+        return(a)
+def uppercase(s1):
+    s1 = list(s1)
+    for i in range(len(s1)):
+        check = islower(s1[i])
+        s1[i] = chr(check)
+    print("".join(s1))
