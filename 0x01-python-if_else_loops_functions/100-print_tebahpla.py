9ee810c70fd3bec3ab1ff95a5bcb5c2ddadcfeee
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-28 10:24:51 +0000

    Removed extra newline at the end

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+j = 0
+for i in range(ord("z"), ord("a") - 1, -1):
+    asc = i
+    if j % 2 == 1:
+        asc -= (ord("a") - ord("A"))
+    j += 1
+    print("{}".format(chr(asc)), end="")
