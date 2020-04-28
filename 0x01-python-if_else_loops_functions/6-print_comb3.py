0b4625434201e6fbfe16744b42b65bb3ce080b00
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-28 09:45:19 +0000

    Uses i to limit the start value of j. Hopefully doesn't count as storing a number

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+for i in range(0, 9):
+    for j in range(i, 10):
+        if j > i and j * i != 8 * 9:
+            print("{}{}".format(i, j), end=", ")
+        elif j * i == 8 * 9:
+            print("{}{}".format(i, j))
