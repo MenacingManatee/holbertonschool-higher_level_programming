b6f0964834a2c65abdfbf68a5012045dab8fafca
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-28 23:11:30 +0000

    Adds test files for task 13

---
+++ 0x01-python-if_else_loops_functions/testfiles/13-main.c
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+#include <limits.h>
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	listint_t *head;
+
+	head = NULL;
+	add_nodeint_end(&head, 0);
+	add_nodeint_end(&head, 1);
+	add_nodeint_end(&head, 2);
+	add_nodeint_end(&head, 3);
+	add_nodeint_end(&head, 4);
+	add_nodeint_end(&head, 98);
+	add_nodeint_end(&head, 402);
+	add_nodeint_end(&head, 1024);
+/*	add_nodeint_end(&head, INT_MAX);*/
+	print_listint(head);
+
+	printf("-----------------\n");
+
+	insert_node(&head, 27);
+
+	insert_node(&head, -1);
+
+	insert_node(&head, 369);
+
+	insert_node(&head, -999);
+
+	insert_node(&head, INT_MAX);
+
+	insert_node(&head, INT_MIN);
+
+	print_listint(head);
+
+	free_listint(head);
+
+	return (0);
+}
