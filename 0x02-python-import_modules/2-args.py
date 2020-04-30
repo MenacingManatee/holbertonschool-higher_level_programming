3bab1db7316dd5853a639ddd60e8c1f015e7831c
MenacingManatee <brett.p.davis101@gmail.com>
2020-04-30 20:57:34 +0000

    I need to do better proofreading

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,10 +5,11 @@ if __name__ == "__main__":
     l = len(sys.argv)
     if l == 1:
         print("0 arguments.")
-    elif l != 2:
-        print("{} arguments:".format(l - 1))
+    elif l == 2:
+        print("1 argument:".format(l - 1))
+        print("1: {}".format(sys.argv[1]))
     else:
-        print("1 argument:")
+        print("{} arguments:".format(l - 1))
         for i in range(l):
             if i != 0:
                 print("{}: {}".format(i, sys.argv[i]))
