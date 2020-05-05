b91f314e3c63effe2eb9768be6f10b4db8fc16a4
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-05 21:24:02 +0000

    Loops through the list, overwriting maxnum var is a bigger number is found

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+def max_integer(my_list=[]):
+    if (my_list):
+        maxnum = my_list[0]
+        for i in range(len(my_list)):
+            if (my_list[i] > maxnum):
+                maxnum = my_list[i]
+        return (maxnum)
+    else:
+        return (None)
