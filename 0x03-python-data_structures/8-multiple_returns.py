6995b05d8aac3cb5a3121e4985c1b1218db8071e
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-05 21:19:53 +0000

    Same as last task, uses a list and converts to tuple on return

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def multiple_returns(sentence):
+    res_list = []
+    res_list.append(len(sentence))
+    if (sentence):
+        res_list.append(sentence[0])
+    else:
+        res_list.append("None")
+    return(tuple(res_list))
