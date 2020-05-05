6b4b98bb0b89136dbf0ac3cae36df1e68d2d4d56
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-05 21:35:27 +0000

    Loops through each lest, checking the % 2 of each item

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
-
+def divisible_by_2(my_list=[]):
+    if (my_list):
+        res_list = []
+        for i in range(len(my_list)):
+            if (my_list[i] % 2 == 0):
+                res_list.append(True)
+            else:
+                res_list.append(False)
+        return (res_list)
