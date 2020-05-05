ce07e92d2ccc15b6fd3153c8f8a57447bab68415
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-05 21:15:30 +0000

    Gets around tuple immutability by using a list and converting to tuple at return

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
-
+def add_tuple(tuple_a=(), tuple_b=()):
+    res_list = []
+    for i in range(0, 2):
+        if (len(tuple_a) <= i):
+            num1 = 0
+        else:
+            num1 = tuple_a[i]
+        if (len(tuple_b) <= i):
+            num2 = 0
+        else:
+            num2 = tuple_b[i]
+        res_list.append(num1 + num2)
+    return (tuple(res_list))
