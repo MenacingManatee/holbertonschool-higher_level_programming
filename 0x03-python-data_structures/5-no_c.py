b46b8fe04c30216929d33a19142b96412158b451
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-05 20:08:07 +0000

    Uses a list to so the variable is mutable, the uses .join to convert to string on return

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def no_c(my_string):
+    if (my_string):
+        new_string = []
+        for i in range(len(my_string)):
+            if (my_string[i] != 'c' and my_string[i] != 'C'):
+                new_string.append(my_string[i])
+        return ("".join(new_string))
+    return ""
