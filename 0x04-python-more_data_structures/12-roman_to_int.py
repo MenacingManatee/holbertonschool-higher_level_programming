3fddf60dbb1624ea21fac4052bd408ba5bc778f2
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 22:36:06 +0000

    Adds basic function TODO: Subtract the value instead of add if a smaller number comes before a larger number i.e. IX should result in 9 instead of 11

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,22 @@
 #!/usr/bin/python3
-
+def roman_to_int(roman_string):
+    if (roman_string is None or type(roman_string) is not str):
+        return (0)
+    sum = 0
+    for i in range(len(roman_string)):
+        num = roman_string[i]
+        if (num is 'M'):
+            sum += 1000
+        elif (num is 'D'):
+            sum += 500
+        elif (num is 'C'):
+            sum += 100
+        elif (num is 'L'):
+            sum += 50
+        elif (num is 'X'):
+            sum += 10
+        elif (num is 'V'):
+            sum += 5
+        else:
+            sum += 1
+    return (sum)
