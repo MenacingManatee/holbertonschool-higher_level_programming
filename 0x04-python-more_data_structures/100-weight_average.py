8b9d8ba287e52698d1abbeba93f617a13c877fc0
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 21:52:19 +0000

    Felt a bit hacky going through the list twice, but gets the total weight first, then multiplies the score by weight percentage

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def weight_average(my_list=[]):
+    if (my_list is None or len(my_list) == 0):
+        return (0)
+    total = 0
+    scores = 0
+    for tup in my_list:
+        scores += tup[1]
+    for tup in my_list:
+        total += (tup[0] * (tup[1] / scores))
+    return (total)
