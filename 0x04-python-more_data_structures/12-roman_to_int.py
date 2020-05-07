52c41c0027a6354827fc317095a113da4038e115
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 22:46:37 +0000

    Switches switch statement to a dictionary search, with conditionals to subtract instead of add

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,21 +2,13 @@
 def roman_to_int(roman_string):
     if (roman_string is None or type(roman_string) is not str):
         return (0)
-    sum = 0
+    numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    sm = 0
     for i in range(len(roman_string)):
         num = roman_string[i]
-        if (num is 'M'):
-            sum += 1000
-        elif (num is 'D'):
-            sum += 500
-        elif (num is 'C'):
-            sum += 100
-        elif (num is 'L'):
-            sum += 50
-        elif (num is 'X'):
-            sum += 10
-        elif (num is 'V'):
-            sum += 5
-        else:
-            sum += 1
-    return (sum)
+        if (num in numerals):
+            if (i + 1 < len(roman_string) and numerals.get(roman_string[i + 1]) > numerals.get(roman_string[i])):
+                sm -= numerals.get(roman_string[i])
+            else:
+                sm += numerals.get(roman_string[i])
+    return (sm)
