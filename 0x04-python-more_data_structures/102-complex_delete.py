f094544317de9843049a0bebd0a202e696aea6fc
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 22:06:49 +0000

    Casts dict to list to force copy of keys while avoiding runtime error for dict changing size while iterating

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def complex_delete(a_dictionary, value):
+    if (a_dictionary is None or len(a_dictionary) == 0):
+        return (a_dictionary)
+    for key in list(a_dictionary):
+        if (a_dictionary.get(key) == value):
+            a_dictionary.pop(key)
+    return (a_dictionary)
