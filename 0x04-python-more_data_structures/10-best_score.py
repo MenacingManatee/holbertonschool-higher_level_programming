3dbd167ae6cb487397845820038fb098f59e5529
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 21:21:51 +0000

    Added a keyRes var to save the key associated with the largest value

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def best_score(a_dictionary):
+    if (a_dictionary == None):
+        return (None)
+    keys = a_dictionary.keys()
+    res = 0
+    for key in keys:
+        if (a_dictionary[key] > res):
+            res = a_dictionary[key]
+            keyRes = key
+    return (keyRes)
