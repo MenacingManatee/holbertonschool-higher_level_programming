932c8efb8bcc05bd14e588a19bbac95e1b3f4fdc
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 22:50:32 +0000

    Updated with pep8 compliance

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,12 +2,15 @@
 def roman_to_int(roman_string):
     if (roman_string is None or type(roman_string) is not str):
         return (0)
-    numerals = {'M': 1000, 'D': 500, 'C': 100, 'L': 50, 'X': 10, 'V': 5, 'I': 1}
+    numerals = {'M': 1000, 'D': 500, 'C': 100,
+                'L': 50, 'X': 10, 'V': 5, 'I': 1}
     sm = 0
     for i in range(len(roman_string)):
         num = roman_string[i]
         if (num in numerals):
-            if (i + 1 < len(roman_string) and numerals.get(roman_string[i + 1]) > numerals.get(roman_string[i])):
+            if (i + 1 < len(roman_string) and
+                numerals.get(roman_string[i + 1]) >
+                    numerals.get(roman_string[i])):
                 sm -= numerals.get(roman_string[i])
             else:
                 sm += numerals.get(roman_string[i])
