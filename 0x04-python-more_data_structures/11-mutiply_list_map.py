a2e744f47fe42f0e67025d8d31fe099bfb7b2e92
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 21:42:24 +0000

    File is now 3 lines instead of just the function

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    resList = list(map((number).__mul__, my_list))
-    return (resList)
+    return (list(map((number).__mul__, my_list)))
