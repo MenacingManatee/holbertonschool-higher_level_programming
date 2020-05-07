ebe2514721ec2e9e12c74415a730d078f67fe9e9
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 08:11:58 +0000

    Uses list methods index, remove, and insert to find and replace on a copy of my_list

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
-
+def search_replace(my_list, search, replace):
+    res_list  = my_list.copy()
+    while search in res_list:
+        x = res_list.index(search)
+        res_list.remove(search)
+        res_list.insert(x, replace)
+    return (res_list)
