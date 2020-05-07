886af3b11289b06a30cdbc8e42b559add6fb9199
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 21:10:12 +0000

    Updates a new dict with the values multiplied by 2

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
-
+def multiply_by_2(a_dictionary):
+    new_dict = {}
+    keys = a_dictionary.keys()
+    for key in keys:
+        new_dict.update({key: a_dictionary.get(key) * 2})
+    return (new_dict)
