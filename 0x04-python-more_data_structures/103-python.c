79ad2fb5842f5801d8ef45043465dc66bcceff18
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 23:13:49 +0000

    Adds print_python_list from last cPython task TODO: Remove banned macros Add print_python_bytes function

--- 0x04-python-more_data_structures/103-python.c
+++ 0x04-python-more_data_structures/103-python.c
@@ -1,12 +1,26 @@
+#include "lists.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * main - 
+ * print_python_list_info - prints some basic info about python lists
+ * @p: PyObject to print info from
  *
- * Return: Always 0 (ok)
+ * Return: Void
  */
-int main(void)
+void print_python_list(PyObject *p)
 {
-	
-	return (0);
+	int sz, i;
+
+	if (!PyList_Check(p))
+		return;
+	sz = PyList_Size(p);
+	printf("[*] Size of the Python List = %d\n", sz);
+	printf("[*] Allocated = %lu\n", ((PyListObject *)p)->allocated);
+	for (i = 0; i < sz; i++)
+	{
+		printf("Element %d: %s\n", i,
+		       Py_TYPE(PyList_GetItem(p, i))->tp_name);/*Need to rm*/
+		Py_INCREF(p);
+	}
 }
