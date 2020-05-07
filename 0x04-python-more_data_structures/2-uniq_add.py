d54d1be712c240f53839b2dfb4423369a10f8abf
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 08:15:57 +0000

    Maintains a running list of unique numbers added, only add if not in this list

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def uniq_add(my_list=[]):
+    uniq_list = []
+    sum = 0
+    for i in range(len(my_list)):
+        if (my_list[i] not in uniq_list):
+            sum += my_list[i]
+            uniq_list.append(my_list[i])
+    return (sum)
