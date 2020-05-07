ad830085791f306b5de455344f4fff9a5644ab5e
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-07 21:40:47 +0000

    Fixed double list and shortened to 3 lines

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def mutiply_list_map(my_list=[], number=0):
-    resList = []
-    resList.append(list(map((number).__mul__, my_list)))
-    return (resList[0])
+    resList = list(map((number).__mul__, my_list))
+    return (resList)
