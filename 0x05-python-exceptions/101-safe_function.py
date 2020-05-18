3954fe23d392a2c78d242f8752b4ee96250f1501
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-18 22:47:14 +0000

    Changes manual grabs of each exception to grabbing every exception

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -3,13 +3,7 @@ def safe_function(fct, *args):
     import sys
     try:
         a = fct(args[0], args[1])
-    except ZeroDivisionError as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        a = None
-    except IndexError as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        a = None
-    except TypeError as e:
+    except Exception as e:
         print("Exception: {}".format(e), file=sys.stderr)
         a = None
     finally:
