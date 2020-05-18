35df561a30ba27acfc25c1f3d9922bf91908bd0e
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-18 22:30:41 +0000

    Prints to stderr for typeError and ValueError

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        res = True
+    except TypeError as e:
+        print(e, file=sys.stderr)
+        res = False
+    except ValueError as e:
+        print(e, file=sys.stderr)
+        res = False
+    return (res)
