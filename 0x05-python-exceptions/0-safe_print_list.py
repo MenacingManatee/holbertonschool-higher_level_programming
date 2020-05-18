f5da4995ff1016590f51a46398705d87f744b8c5
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-18 07:22:17 +0000

    Uses a pass command in try/except to ignore errors

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+def safe_print_list(my_list=[], x=0):
+    printed = 0
+    for i in range(0, int(x)):
+        try:
+            print("{}".format(my_list[i]), end="")
+            printed += 1
+        except:
+            pass
+    print("")
+    return (printed)
