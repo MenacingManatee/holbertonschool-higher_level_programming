ceb4a82d944422353083243603ebdf648364aa2d
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 21:04:54 +0000

    Adds documentation to task 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+
+'''Square class'''
+
+
 class Square:
+    '''Initialize a square class with size and area
+Size has a getter and setter function'''
     __size = 0
 
     def area(self):
+        '''Returns the area of the square'''
         return (self.__size ** 2)
 
     def __init__(self, size=0):
+        '''Initializes size'''
         self.__size = size
 
     @property
     def size(self):
+        '''Returns size'''
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        '''Sets/resets size, with error raised on bad value'''
         if (type(value) is int):
             if (value >= 0):
                 self.__size = value
