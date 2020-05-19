7099e3d8df8228147e80f1fd3c2b54e105e81afe
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 21:09:39 +0000

    Adds documentation to task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,20 +1,30 @@
 #!/usr/bin/python3
+
+'''Square class'''
+
+
 class Square:
+    '''Defines a square class with the attributes size,
+area, and position, with the ability to print the square visually'''
     __size = 0
 
     def area(self):
+        '''Gets the area of the square'''
         return (self.__size ** 2)
 
     def __init__(self, size=0, position=(0, 0)):
+        '''Initializes values for size and position'''
         self.position = position
         self.__size = size
 
     @property
     def size(self):
+        '''Getter for size'''
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        '''Setter for size'''
         if (type(value) is int):
             if (value >= 0):
                 self.__size = value
@@ -25,10 +35,12 @@ class Square:
 
     @property
     def position(self):
+        '''Getter for position'''
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        '''Setter for position'''
         if (isinstance(value, tuple) and len(value) is 2 and
                 len([i for i in value if type(value) is tuple and i >= 0]) is
                 2):
@@ -37,6 +49,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
 
     def my_print(self):
+        '''Prints a square visually at position provided'''
         for i in range(self.__position[1]):
             print("")
         for i in range(self.__size):
