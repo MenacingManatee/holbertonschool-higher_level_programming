bfaea183230aa78f77d6e560b26e53e0e5967450
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 21:54:31 +0000

    Shouldn't change anything, but slight change to docstrings

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -4,9 +4,9 @@ import math
 
 
 class MagicClass:
-    '''Defines a class that includes the radius, area, etc of a circle'''
     def __init__(self, radius=0):
-        '''initializes radius'''
+        '''initializes radius
+        '''
         self.__radius = 0
         if (type(radius) is not int and type(radius) is not float):
             raise TypeError('radius must be a number')
