b0daccb5cf5878fe14844e7155bd72ab470b3294
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 08:39:04 +0000

    Adds the 2 error raises for value and type errors

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
-
+class Square:
+    __size = 0
+    def __init__(self, size=0):
+        if (type(size) is int):
+            if (size >= 0):
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
