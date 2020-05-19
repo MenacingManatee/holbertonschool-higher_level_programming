ab3b8380e1d7ee521803f8861ee64156785747ca
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 21:13:32 +0000

    Adds documentation to advanced task 101

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,20 +1,28 @@
 #!/usr/bin/python3
+
+'''Square class'''
 class Square:
+    '''Defines a class square with area, size, position, and the ability
+to visually print. Returns a string of the visual square'''
     __size = 0
 
     def area(self):
+        '''Area getter'''
         return (self.__size ** 2)
 
     def __init__(self, size=0, position=(0, 0)):
+        '''Initializes size and position'''
         self.position = position
         self.__size = size
 
     @property
     def size(self):
+        '''size getter'''
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        '''size setter'''
         if (type(value) is int):
             if (value >= 0):
                 self.__size = value
@@ -25,10 +33,12 @@ class Square:
 
     @property
     def position(self):
+        '''position getter'''
         return (self.__position)
 
     @position.setter
     def position(self, value):
+        '''position setter'''
         if (isinstance(value, tuple) and len(value) is 2 and
                 len([i for i in value if type(value) is tuple and i >= 0]) is
                 2):
@@ -37,6 +47,7 @@ class Square:
             raise TypeError('position must be a tuple of 2 positive integers')
 
     def my_print(self):
+        '''prints a square visually at position to terminal'''
         for i in range(self.__position[1]):
             print("")
         for i in range(self.__size):
@@ -47,6 +58,7 @@ class Square:
             print("")
 
     def strsqr(self):
+        '''Returns a string representation of the square'''
         res = []
         for i in range(self.__position[1]):
             res.append('\n')
@@ -60,4 +72,5 @@ class Square:
         return ("".join(res))
 
     def __repr__(self):
+        '''Defines return value of Square class to be strsqr'''
         return (self.strsqr())
