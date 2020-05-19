8ad3cf2a6dec21b9acdc11bb32e8139fac1d6b1c
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 08:50:00 +0000

    Not sure if the self.area bit is necessary here considering it just returns the area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
-
+class Square:
+    __size = 0
+    area = 0
+    def area(self):
+        self.area = self.__size ** 2
+        return (self.area)
+    def __init__(self, size=0):
+        if (type(size) is int):
+            if (size >= 0):
+                self.__size = size
+            else:
+                raise ValueError('size must be >= 0')
+        else:
+            raise TypeError('size must be an integer')
