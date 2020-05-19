8f47e0ab3452dcad245a22ce69ea055de071e299
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-19 21:18:07 +0000

    Adds logical operator functionality to task 4

--- 0x06-python-classes/102-square.py
+++ 0x06-python-classes/102-square.py
@@ -1,19 +1,29 @@
 #!/usr/bin/python3
+
+'''Square class'''
+
+
 class Square:
+    '''Defines a square class with logical operators available based on area,
+as well as size and area'''
     __size = 0
 
     def area(self):
+        '''area getter'''
         return (self.__size ** 2)
 
     def __init__(self, size=0):
+        '''Initializes size'''
         self.__size = size
 
     @property
     def size(self):
+        '''size getter'''
         return (self.__size)
 
     @size.setter
     def size(self, value):
+        '''size setter'''
         if (type(value) is int):
             if (value >= 0):
                 self.__size = value
@@ -23,19 +33,25 @@ class Square:
             raise TypeError('size must be an integer')
 
     def __eq__(self, other):
+        '''Sets __eq__ to check area'''
         return (self.area() == other.area())
 
     def __lt__(self, other):
+        '''Sets __lt__ to check area'''
         return (self.area() < other.area())
 
     def __gt__(self, other):
+        '''Sets __gt__ to check area'''
         return (self.area() > other.area())
 
     def __ne__(self, other):
+        '''Sets __ne__ to check area'''
         return (not (self.area() == other.area()))
 
     def __le__(self, other):
+        '''Sets __le__ to check area'''
         return (self.area() <= other.area())
 
     def __ge__(self, other):
+        '''Sets __ge__ to check area'''
         return (self.area() >= other.area())
