d414d272a285851a394d82e47486949185fca80d
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-20 16:29:57 +0000

    Changes to a more pythonic, shorter method

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -4,11 +4,12 @@
 
 
 class Square:
-    '''Defines a square with size, area, and the ability to print'''
+    '''Initialize a square class with size and area
+Size has a getter and setter function'''
     __size = 0
 
     def area(self):
-        '''Returns area of a square'''
+        '''Returns the area of the square'''
         return (self.__size ** 2)
 
     def __init__(self, size=0):
@@ -17,12 +18,12 @@ class Square:
 
     @property
     def size(self):
-        '''Getter for size'''
+        '''Returns size'''
         return (self.__size)
 
     @size.setter
     def size(self, value):
-        '''Setter for size'''
+        '''Sets/resets size, with error raised on bad value'''
         if (type(value) is int):
             if (value >= 0):
                 self.__size = value
@@ -32,8 +33,9 @@ class Square:
             raise TypeError('size must be an integer')
 
     def my_print(self):
-        '''Prints a square visually to the terminal'''
-        for i in range(self.__size):
-            print("#" * self.__size)
-        if (self.__size == 0):
-            print("")
+        '''Prints out a visual representation of square
+        using string.join() method and list comprehension'''
+        if self.__size is 0:
+            print()
+        print("".join(['#' * self.__size + '\n' for i in range(self.__size)]),
+              end="")
