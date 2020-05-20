9d665355742d35e07d749fc17d732eb85052699d
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-20 01:48:17 +0000

    Cleaned up logic, removed legacy pass statement, fixed instances of bad attribute calls

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -13,59 +13,58 @@ class Node:
         '''returns data'''
         return (self.__data)
 
+    @data.setter
+    def data(self, value):
+        '''setter for data'''
+        if (type(value) is not int):
+            raise TypeError('data must be an integer')
+        self.__data = value
+
     @property
     def next_node(self):
         '''Returns next node in list'''
         return (self.__next_node)
 
-    @data.setter
-    def data(self, value):
-        '''setter for data'''
-        if (not isinstance(value, int)):
-            raise TypeError('data must be an integer')
-        self.__data = value
-
     @next_node.setter
     def next_node(self, value):
         '''setter for next node'''
-        if not (isinstance(value, Node) or isinstance(value, None)):
+        if (value is not None and not isinstance(value, Node)):
             raise TypeError('next_node must be a Node object')
         self.__next_node = value
 
 class SinglyLinkedList:
     '''Contains a chain of linked nodes'''
     def __init__(self):
+        '''Initialization of head'''
         self.__head = None
-        '''initialization'''
-        pass
-
-    def head(self, val):
-        '''sets head to val'''
-        self.__head = val
 
     def sorted_insert(self, value):
         '''inserts a node at a sorted position'''
         hare = self.__head
         tortoise = hare
+        if (hare is None):
+            self.__head = Node(value, None)
+            return
+        if (hare.data > value):
+            self.__head = Node(value, hare)
+            return
         while (hare is not None and hare.data < value):
             tortoise = hare
-            hare = hare.__next_node
-        newNode = Node
-        newNode.data = value
-        newNode.__next_node = hare
-        if (tortoise is not None):
-            tortoise.__next_node = newNode
-        else:
-            self.__head = newNode
+            hare = hare.next_node
+
+        tortoise.next_node = Node(value, hare)
 
     def liststr(self):
         '''makes a string of the list to be returned'''
         res = []
         hare = self.__head
+        if (hare is None):
+            return ("")
         while (hare is not None):
-            res.append(hare.data)
-            res.append('\n')
-            hare = hare.__next_node
+            res.append(str(hare.data))
+            if (hare.next_node is not None):
+                res.append('\n')
+            hare = hare.next_node
         return ("".join(res))
 
     def __repr__(self):
