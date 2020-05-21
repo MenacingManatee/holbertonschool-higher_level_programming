289eee309cd353af42d78c570c78ba39aad8330e
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-21 23:24:29 +0000

    Adds task 4 after passing all tests

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+'''Prints a string, adding two newlines after each of the following:
+'.', '?', and ':'
+Text must be a string'''
 
+
+def text_indentation(text):
+    '''Usage: text_indentation(text)'''
+    if not isinstance(text, str):
+        raise TypeError('text must be a string')
+    flag = 0
+    for char in text:
+        if flag is 1 and char is ' ':
+            continue
+        print(char, end="")
+        flag = 0
+        if char in ['.', ':', '?']:
+            print('\n')
+            flag = 1
