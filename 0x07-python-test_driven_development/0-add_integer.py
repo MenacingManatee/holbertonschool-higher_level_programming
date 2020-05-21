8a12bd89722da94ce9b546034fae5e6b9b078ca4
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-21 08:48:48 +0000

    Updates documentation, adds more tests to doctest for 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+'''Adds two integers
+   If floats are sent, casts to int before adding'''
+
+
 def add_integer(a, b=98):
+    '''Usage: add_integer(a, b=98)'''
     if not isinstance(a, int) and not isinstance(a, float):
         raise TypeError('a must be an integer')
     elif not isinstance(b, int) and not isinstance(b, float):
