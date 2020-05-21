4e249303dbd76f59a2eca905cea2dfbeb46453ad
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-21 21:59:02 +0000

    Adding program after all tests have passed

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,2 +1,19 @@
 #!/usr/bin/python3
+'''Prints a square in a visual format using
+the '#' symbol
+Example: print_square(2)
+##
+##
+'''
 
+
+def print_square(size):
+    '''Usage: print_square(size)'''
+    if not isinstance(size, int):
+        raise TypeError('size must be an integer')
+    elif size < 0:
+        raise TypeError('size must be >= 0')
+    elif size is 0:
+        print("")
+    else:
+        print("".join(['#' * size + '\n' for i in range(size)]), end="")
