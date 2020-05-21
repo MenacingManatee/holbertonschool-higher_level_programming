5848f466bdfee47a83ff4399b81ccab766b93f7e
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-21 21:36:53 +0000

    Adds file with documentation and passed tests for task 2

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+'''Prints a user-defined first and last name in the format:
+My name is <first name> <last name>
+Last name is not a required variable'''
 
+
+def say_my_name(first_name, last_name=""):
+    '''Usage: say_my_name(first_name, last_name="")'''
+    if not isinstance(first_name, str):
+        raise TypeError('first_name must be a string')
+    elif not isinstance(last_name, str):
+        raise TypeError('last_name must be a string')
+    else:
+        print("My name is {} {}".format(first_name, last_name))
