408165e19e17a7f63a977e5642d75510b5f19247
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-23 03:54:39 +0000

    Adds task file after passing all tests created

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -16,9 +16,32 @@ def matrix_mul(m_a, m_b):
         raise TypeError('m_b must be a list of lists')
     elif (len([i for i in range(len(m_a)) if m_a[i]]) != len(m_a)):
         raise ValueError('m_a can\'t be empty')
-    elif (len([i for i in range(len(m_b)) if m_a[i]]) != len(m_b)):
+    elif (len([i for i in range(len(m_b)) if m_b[i]]) != len(m_b)):
         raise ValueError('m_b can\'t be empty')
     elif (len([i for i in range(len(m_a)) if all(isinstance(j, (int, float))
                                                  for j in m_a[i])]
-          ) != len(m_a)):
+              ) != len(m_a)):
         raise TypeError('m_a should contain only integers or floats')
+    elif (len([i for i in range(len(m_b)) if all(isinstance(j, (int, float))
+                                                 for j in m_b[i])]
+              ) != len(m_b)):
+        raise TypeError('m_b should contain only integers or floats')
+    elif (len([i for i in range(len(m_a)) if len(m_a[i]) == len(m_a[0])])
+          != len(m_a)):
+        raise TypeError('each row of m_a must be of the same size')
+    elif (len([i for i in range(len(m_b)) if len(m_b[i]) == len(m_b[0])])
+          != len(m_b)):
+        raise TypeError('each row of m_b must be of the same size')
+    elif (len(m_a[0]) != len(m_b)):
+        raise ValueError('m_a and m_b can\'t be multiplied')
+    else:
+        return (matrix_multiplier(m_a, m_b))
+
+def matrix_multiplier(m_a, m_b):
+    '''Driver function for matrix_mul'''
+    res = [ [0 for i in range(len(m_a[0]))] for j in range(len(m_a)) ]
+    for i in range(len(m_a)):
+        for j in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                res[i][j] += m_a[i][k] * m_b[k][j]
+    return (res)
