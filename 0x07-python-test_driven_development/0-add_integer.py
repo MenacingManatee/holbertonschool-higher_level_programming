aecbfffbea6e2dacb28fafe9e1e1ffe203162464
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-25 00:59:07 +0000

    Added new tests for missing arguments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -5,8 +5,11 @@
 
 def add_integer(a, b=98):
     '''Usage: add_integer(a, b=98)'''
-    if not isinstance(a, int) and not isinstance(a, float):
+    if (a == float("inf") or (not isinstance(a, int) and not
+                              isinstance(a, float)) or a != a):
         raise TypeError('a must be an integer')
-    elif not isinstance(b, int) and not isinstance(b, float):
+    elif (b == float("inf") or (not isinstance(b, int) and not
+                                isinstance(b, float)) or b != b or
+            b is float("inf")):
         raise TypeError('b must be an integer')
     return (int(a) + int(b))
