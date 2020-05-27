96f760120647dd9128143b1fd797efd0e251c962
MenacingManatee <brett.p.davis101@gmail.com>
2020-05-27 00:21:59 +0000

    Same file, but removed documentation and unnecessary var to get the required linecount

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,10 +1,3 @@
 #!/usr/bin/python3
-'''Copies a list'''
-
-
 def copy_list(l):
-    '''Usage: copy_list(list)
-    Returns a copy of the list'''
-    r = []
-    r = l.copy()
-    return r
+    return l.copy()
