842a32a8183969a3c503bf30bfe7f79c32ee8d97
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 08:06:03 +0000

    uses the list sort() method on a copy

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+'''Defines a class MyList that inherits from list'''
 
+
+class MyList(list):
+    def print_sorted(self):
+        tmp = self.copy()
+        tmp.sort()
+        print(tmp)
