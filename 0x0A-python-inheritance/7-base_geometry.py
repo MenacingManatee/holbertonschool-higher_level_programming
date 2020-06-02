a8c7e15561066f766e8609a9b2a896c29275626d
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 09:22:23 +0000

    Adds checks for type and value

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+'''Defines an empty class, BaseGeometry'''
 
+
+class BaseGeometry():
+    '''Defines public method area() that is not yet implemented'''
+    def area(self):
+        '''Not yet implemented'''
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        elif value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
