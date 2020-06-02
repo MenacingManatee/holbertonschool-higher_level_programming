595e2c3eccb297b68878802340f9cb4dfb2f0d12
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 20:28:02 +0000

    Checks for __slots__ and __dict__, and assumes the class can't be added to if they can't be found

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Defines a function that adds a new attribute to an object if possible'''
+
+
+def add_attribute(obj, name, value):
+    '''Usage: add_attribute(object, attribute_name, attribute_value)'''
+    if ((hasattr(obj, '__slots__') and name in obj.__slots__) or hasattr(obj, '__dict__')):
+        setattr(obj, name, value)
+    else:
+        raise TypeError('can\'t add new attribute')
