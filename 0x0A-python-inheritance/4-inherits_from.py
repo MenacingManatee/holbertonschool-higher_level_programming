354d9321d32869d2bbdd29ac89cbd46c37dabcf8
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 08:34:26 +0000

    Uses issubclass and a check to make sure they aren't the same

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''Finds f the object is an instance of a class that inherited 
+(directly or indirectly) from the specified class
+'''
 
+
+def inherits_from(obj, a_class):
+    '''Usage: inherits_from(obj, class)'''
+    if type(obj) is not a_class:
+        return issubclass(type(obj), a_class)
+    return False
