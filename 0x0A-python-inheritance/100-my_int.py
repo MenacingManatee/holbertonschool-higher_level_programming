65932cd692169b4ad210d74cc89189dc8d90c5b7
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 19:57:30 +0000

    Fixed pep8 formatting, added docstrings

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,8 +3,12 @@
 
 
 class MyInt(int):
+    '''Class reverses the != and == operators of int'''
+
     def __eq__(self, other):
+        '''defines the eq operator as !='''
         return int(self) != other
 
     def __ne__(self, other):
+        '''defines the ne operator as =='''
         return int(self) == other
