60aa1640947a3426ebbb8bc9b8c152e7a244ecb8
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 08:40:26 +0000

    When area is called, the appropriate exception is raised

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+'''Defines an empty class, BaseGeometry'''
 
+
+class BaseGeometry():
+    '''Defines public method area() that is not yet implemented'''
+    def area(self):
+        '''Not yet implemented'''
+        raise Exception('area() is not implemented')
