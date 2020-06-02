1099e8b600a76bd36b982bc3d9e43006313afb8b
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 07:55:30 +0000

    No checks since everything is an object Returns the return value of dir on the object

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+'''Defines a function lookup that finds and returns a list of
+available attributes and methods for an object'''
 
+
+def lookup(obj):
+    '''Usage: lookup(obj)'''
+    return(dir(obj))
