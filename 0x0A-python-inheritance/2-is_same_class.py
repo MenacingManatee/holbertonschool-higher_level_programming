774f60ee1d5640a3bcb81e878c5ff0ecad16121b
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-02 08:25:17 +0000

    Swapped from isinstance to type because isinstance returned True on object

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,2 +1,7 @@
 #!/usr/bin/python3
+'''Finds if the object is exactly an instance of the specified class'''
 
+
+def is_same_class(obj, a_class):
+    '''Usage: is_same_class(obj, class)'''
+    return type(obj) is a_class
