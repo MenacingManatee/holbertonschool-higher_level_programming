bbc67607e2138c2bb0b0429a2fa92d37e018ede3
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 21:20:33 +0000

    The try/except bit feels hacky, but it works

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+'''Defines a script that adds all arguments to a Python list,
+ and then save them to a file:'''
 
+
+import sys
+save = __import__('7-save_to_json_file').save_to_json_file
+load = __import__('8-load_from_json_file').load_from_json_file
+try:
+    l1 = load('add_item.json')
+except:
+    l1 = []
+l2 = list(sys.argv[1:])
+l1 = l1 + l2
+save(l1, 'add_item.json')
