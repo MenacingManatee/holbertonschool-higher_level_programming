e952c54fa4dbc7aa1b6038e202651d48d74b1b9d
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 17:29:25 +0000

    There's probably a better way, but counting the newlines works

--- 0x0B-python-input_output/1-number_of_lines.py
+++ 0x0B-python-input_output/1-number_of_lines.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-
+def number_of_lines(filename=""):
+    count = 0
+    with open(filename) as f:
+        e = f.read()
+        for i in e:
+            if i == '\n':
+                count += 1
+    return count
