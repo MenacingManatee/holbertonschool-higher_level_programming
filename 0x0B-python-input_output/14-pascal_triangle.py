e16377d92e2d4bfe2c3000f3ac3dc1f35185e620
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 23:08:58 +0000

    Uses multiple if statements to determine if the list would be out of range, defaulting to 0 if it would be Then adds previous list items to make new list item

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -1,2 +1,26 @@
 #!/usr/bin/python3
+'''Defines a function that returns a list of lists of integers
+ representing the Pascals triangle of n'''
 
+
+def pascal_triangle(n):
+    '''Usage: pascal_triangle(n)'''
+    if n <= 0:
+        return []
+    res = []
+    res.append([1])
+    tmp = []
+    for i in range(1, n):
+        for j in range(i + 1):
+            if j - 1 < 0:
+                num = 0
+            else:
+                num = res[i - 1][j - 1]
+            if j > i - 1:
+                num += 0
+            else:
+                num += res[i - 1][j]
+            tmp.append(num)
+        res.append(tmp.copy())
+        tmp.clear()
+    return res
