5fccad7cd50b47bda179600d86411c7386dd1c56
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 00:57:18 +0000

    Adds a docstring to init

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -22,6 +22,7 @@ class BaseGeometry():
 class Rectangle(BaseGeometry):
     '''Defines a rectangle with a width and height'''
     def __init__(self, width, height):
+        '''Initializes a rectangle with width and height'''
         self.integer_validator("width", width)
         self.integer_validator("height", height)
         self.__width = width
