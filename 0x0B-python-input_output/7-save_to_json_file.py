e284a87527fc0e9703ca188448775f18c51ca387
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 20:33:11 +0000

    Combines the write and dumps methods we learned previously to write a json object to a file

--- 0x0B-python-input_output/7-save_to_json_file.py
+++ 0x0B-python-input_output/7-save_to_json_file.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''Defines a function that writes an Object to a text file,
+ using a JSON representation'''
 
+
+def save_to_json_file(my_obj, filename):
+    '''Usage: save_to_json_file(my_obj, filename)'''
+    with open(filename, "w") as f:
+        import json
+        f.write(json.dumps(my_obj))
+    f.close
