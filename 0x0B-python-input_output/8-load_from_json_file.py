d01de729f90a1b6a5190ca141f26f44228e79b9c
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 20:37:02 +0000

    Uses the read and loads methods learned previously to return an object from a json file

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,2 +1,10 @@
 #!/usr/bin/python3
+'''Defines a function that creates an Object from a JSON file'''
 
+
+def load_from_json_file(filename):
+    '''Usage: load_from_json_file(filename)'''
+    with open(filename) as f:
+        import json
+        r = f.read()
+        return json.loads(r)
