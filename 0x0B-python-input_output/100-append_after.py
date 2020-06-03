1f4596b4eb9500c51c461d1ceb174f84cd038983
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 23:45:29 +0000

    Not actually appending, but rewriting the file with the append added in

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,1 +1,19 @@
 #!/usr/bin/python3
+'''Defines a function that inserts a line of text to a file,
+ after each line containing a specific string'''
+
+
+def append_after(filename="", search_string="", new_string=""):
+    '''Usage: append_after(filename="", search_string="", new_string="")'''
+    with open(filename, "r") as f:
+        res = []
+        s = f.readline()
+        while (s != ""):
+            res.append(s)
+            if search_string in s:
+                res.append(new_string)
+            s = f.readline()
+    f.close()
+    with open(filename, "w") as f:
+        f.write("".join(res))
+    f.close()
