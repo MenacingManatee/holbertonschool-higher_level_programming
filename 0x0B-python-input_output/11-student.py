eeb116594cb4485d9ed6cfbd3aac0b5b3d946d39
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 21:55:22 +0000

    dumps the dict then returns the load

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
+'''Defines a class Student that defines a student by:
+first name, last name, age
+Methods:
+to_json that retrieves a dictionary representation of a Student instance'''
 
+
+class Student:
+    '''Definition for a student'''
+    def __init__(self, first_name, last_name, age):
+        '''Instantiation with first_name, last_name and age'''
+        self.age = age
+        self.last_name = last_name
+        self.first_name = first_name
+
+
+    def to_json(self):
+        '''retrieves a dictionary representation of a Student instance '''
+        import json
+        d = json.dumps(self.__dict__)
+        return json.loads(d)
