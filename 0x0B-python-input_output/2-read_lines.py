3da42ad27cf6c1044ae88aa5bc1a69954a1bba9a
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 17:37:54 +0000

    Added 'end=""' to print statements to prevent additional newlines

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,2 +1,15 @@
 #!/usr/bin/python3
+'''defines a function that reads n lines of a text file
+(UTF8) and prints it to stdout'''
 
+
+def read_lines(filename="", nb_lines=0):
+    with open(filename) as f:
+        if nb_lines <= 0:
+            print(f.read(), end="")
+        else:
+            for i in range(nb_lines):
+                e = f.readline()
+                if e == "":
+                    break
+                print(e, end="")
