9a2346481c8eedfdc58e4c143fd091edde2228d0
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 17:52:24 +0000

    Uses the 'a' flag to append and create

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''Defines a function that appends a string to a text file (UTF8)
+ and returns the number of characters written:'''
 
+
+def append_write(filename="", text=""):
+    '''Usage: append_write(filename="", text="")'''
+    with open(filename, "a") as f:
+        f.write(text)
+    f.close()
+    return len(text)
