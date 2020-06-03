7ad1eb01a4fb51a55f4b5f593435aab2a895b344
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 20:28:07 +0000

    Uses json.loads() method to load from json string

--- 0x0B-python-input_output/6-from_json_string.py
+++ 0x0B-python-input_output/6-from_json_string.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
+'''Defines a function that returns an object (Python data structure)
+ represented by a JSON string'''
 
+
+def from_json_string(my_str):
+    '''Usage: from_json_string(my_str)'''
+    import json
+    return json.loads(my_str)
