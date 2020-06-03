f303c56ed0c91cafe5f8b2ad3964d72c98ac2d10
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 20:24:47 +0000

    Uses the json.dumps() method to convert

--- 0x0B-python-input_output/5-to_json_string.py
+++ 0x0B-python-input_output/5-to_json_string.py
@@ -1,2 +1,8 @@
 #!/usr/bin/python3
+'''Defines a function that returns the JSON representation of an object
+(string)'''
 
+
+def to_json_string(my_obj):
+    import json
+    return json.dumps(my_obj)
