5d1b8856de64281d07a7ffedd0f51fbe85d477e6
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-03 21:40:56 +0000

    Calls json.dumps() on the __dict__ dunder method of the object

--- 0x0B-python-input_output/10-class_to_json.py
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
+'''Defines a function that returns the dictionary description with simple data
+ structure (list, dictionary, string, integer and boolean)
+ for JSON serialization of an object'''
 
+
+
+def class_to_json(obj):
+    '''Usage: class_to_json(obj)'''
+    import json
+    return json.dumps(obj.__dict__)
