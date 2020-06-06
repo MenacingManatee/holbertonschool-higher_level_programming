4e5e43148ff7623892acb88408c4bb01b0635c3d
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-06 01:54:56 +0000

    Fixed pep8 formatting

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,6 +5,7 @@
 class Base:
     '''Defines the the id, based on the number of objects'''
     __nb_objects = 0
+
     def __init__(self, id=None):
         '''initializes self.id based on the id sent, or number of objects'''
         if id is None:
