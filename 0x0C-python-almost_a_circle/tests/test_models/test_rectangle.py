67faf8b396fab8c6918bb14eefbeac03877c15e8
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-06 00:44:08 +0000

    Adds test cases for task 3

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -26,17 +26,17 @@ class test_rectangle_2(unittest.TestCase):
     def test_id_auto(self):
         '''Tests automatic assignment of IDs
         Starts at 6 due to 1-5 being used in Base tests'''
-        r1 = Rectangle(0, 0)
-        r2 = Rectangle(0, 0)
-        r3 = Rectangle(0, 0)
+        r1 = Rectangle(1, 1)
+        r2 = Rectangle(1, 1)
+        r3 = Rectangle(1, 1)
         self.assertTrue(r1.id == 6)
         self.assertTrue(r2.id == 7)
         self.assertTrue(r3.id == 8)
 
     def test_id_manual(self):
         '''Tests manual assignment of IDs'''
-        r1 = Rectangle(0, 0, 0, 0, 0)
-        r2 = Rectangle(0, 0, 0, 0, 100)
+        r1 = Rectangle(1, 1, 1, 1, 0)
+        r2 = Rectangle(1, 1, 1, 1, 100)
         self.assertTrue(r1.id == 0 and r2.id == 100)
 
     def test_attributes(self):
@@ -47,6 +47,34 @@ class test_rectangle_2(unittest.TestCase):
 
     def test_change_attrs(self):
         '''Tests that attributes are changed correctly'''
-        r1 = Rectangle(0, 0, 0, 0, 1)
+        r1 = Rectangle(1, 1, 1, 1, 1)
         r1.width = 100
         self.assertTrue(r1.width == 100)
+
+
+class test_rectangle_3(unittest.TestCase):
+    '''Defines test cases for the changes to rectangle made in task 3'''
+    def test_exception_type(self):
+        '''Tests to make sure typeerror is raised on various wrong types
+        Also tests the error message when raised'''
+        with self.assertRaises(TypeError) as e:
+            Rectangle(1, '3')
+        self.assertEqual('height must be an integer', str(e.exception))
+        with self.assertRaises(TypeError) as e:
+            Rectangle(float("inf"), 3)
+        self.assertEqual('width must be an integer', str(e.exception))
+        with self.assertRaises(TypeError) as e:
+            Rectangle(1, 3, float("NaN"), 0)
+        self.assertEqual('x must be an integer', str(e.exception))
+        with self.assertRaises(TypeError) as e:
+            Rectangle(1, 3, 0, 0.1)
+        self.assertEqual('y must be an integer', str(e.exception))
+
+    def test_exception_val(self):
+        '''Tests for value error with message'''
+        with self.assertRaises(ValueError) as e:
+            Rectangle(0, 0, 0, 0, 0)
+        self.assertEqual('width must be > 0', str(e.exception))
+        with self.assertRaises(ValueError) as e:
+            Rectangle(1, 1, -1, 0, 0)
+        self.assertEqual('x must be >= 0', str(e.exception))
