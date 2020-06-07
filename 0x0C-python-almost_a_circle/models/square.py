7520394bcef3a56fde1008d74ffe715bc3fc1092
MenacingManatee <brett.p.davis101@gmail.com>
2020-06-07 22:21:43 +0000

    Creates a square class that inherits from rectangle

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python3
+'''defines the class Square that inherits from Rectangle'''
+from models.rectangle import Rectangle
 
+
+class Square(Rectangle):
+    '''Defines a square as a special rectangle with width and height being equal
+    '''
+    def __init__(self, size, x=0, y=0, id=None):
+        '''Initializes a square as a special rectangle'''
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        '''Returns a string description of the square'''
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width)
