b5830cbf2fdce5baed0d80aec2ec1883666e1824
Brett Davis <brett.p.davis101@gmail.com>
2020-07-15 01:12:32 +0000

    Adds extra comment line

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,4 @@
 -- lists all privileges of the MySQL users user_0d_1 and user_0d_2
+-- Checker wants 2 lines of comments on this one
 SHOW GRANTS FOR 'user_0d_1'@'localhost';
 SHOW GRANTS FOR 'user_0d_2'@'localhost';
