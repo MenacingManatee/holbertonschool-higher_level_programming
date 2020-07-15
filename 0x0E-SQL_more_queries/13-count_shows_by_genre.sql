d870a6c67724e6d729b887a85d5303174cfb2380
Brett Davis <brett.p.davis101@gmail.com>
2020-07-15 03:16:59 +0000

    Excludes genres with 0 titles

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -8,4 +8,5 @@ ON tv_show_genres.genre_id=tv_genres.id
 LEFT JOIN tv_shows
 ON tv_shows.id=tv_show_genres.show_id
 GROUP BY genre_id, genre
+HAVING number_of_shows > 0
 ORDER BY number_of_shows DESC;
