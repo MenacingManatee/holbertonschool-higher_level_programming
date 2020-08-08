018cdb32f2fcc4adc09c9feafe99933944c1aa96
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 01:29:06 +0000

    Prevents all sql injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,37 +1,22 @@
 #!/usr/bin/python3
-'''lists all states from the database hbtn_0e_0_usa,
-in ascending order by state id'''
+'''lists all states from the database hbtn_0e_0_usa'''
 
 
 if __name__ == "__main__":
-    import sqlalchemy
     import MySQLdb
-    from sqlalchemy import create_engine
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.ext.declarative import declarative_base
     from sys import argv
-    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2]+'\
-@localhost:3306/'+argv[3])
-    conc = engine.connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
+    conc = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
-    cmds = ""
-    if argv[4] is not None:
-        cmds = argv[4].split(';')
-        cmds = cmds[0].strip("'")
+    curs = conc.cursor()
+    cmd = argv[4].split(';')
+    state = cmd[0].strip("'")
 
-    class states(Base):
-        '''Table defining states'''
-        __tablename__ = 'states'
+    curs.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;".format(
+        state))
+    data = curs.fetchall()
 
-        id = Column(Integer, autoincrement=True, primary_key=True)
-        name = Column(String(256))
+    for item in data:
+        print(item)
 
-    for id, name in session.query(states.id, states.name).\
-        filter(states.name ==
-               "{}".format(cmds)).\
-            order_by(states.id):
-        print("({}, '{}')".format(id, name))
+    curs.close()
+    conc.close()
