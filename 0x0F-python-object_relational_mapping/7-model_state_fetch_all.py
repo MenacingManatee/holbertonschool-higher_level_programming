aef96a58af945d2ba1fc373f0366563ba04ad34b
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 00:06:14 +0000

    Uses relationship mapping to retrieve all states

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa,
+in ascending order by state id'''
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    import MySQLdb
+    from sqlalchemy import create_engine
+    from sqlalchemy import Column, Integer, String
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                           '@localhost:3306/'+argv[3])
+    conc = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base = declarative_base()
+
+    class states(Base):
+        '''Table defining states'''
+        __tablename__ = 'states'
+
+        id = Column(Integer, autoincrement=True, primary_key=True)
+        name = Column(String(256))
+
+    for id, name in session.query(states.id, states.name).order_by(
+            states.id):
+        print("{}: {}".format(id, name))
