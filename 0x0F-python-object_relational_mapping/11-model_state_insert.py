8dcdcc04d9dbfbdc00b79185d1fa5981d02cef01
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 03:29:55 +0000

    Adds and commits a new state to the table

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa,
+in ascending order by state id'''
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import MetaData
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                           '@localhost:3306/'+argv[3])
+    metadata = MetaData()
+    conc = engine.connect()
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    newstate = State(name="Louisiana")
+
+    session.add(newstate)
+    session.commit()
+
+    session.close()
