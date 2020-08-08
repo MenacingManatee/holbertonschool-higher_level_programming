f708860db5bf62e28c7260c1da7dd84b59dcc992
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 01:26:52 +0000

    Not safe from SQL injection

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,32 +1,20 @@
 #!/usr/bin/python3
-'''lists all states from the database hbtn_0e_0_usa,
-in ascending order by state id'''
+'''lists all states from the database hbtn_0e_0_usa'''
 
 
 if __name__ == "__main__":
-    import sqlalchemy
     import MySQLdb
-    from sqlalchemy import create_engine
-    from sqlalchemy import Column, Integer, String
-    from sqlalchemy.orm import sessionmaker
-    from sqlalchemy.ext.declarative import declarative_base
     from sys import argv
-    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2]+'\
-@localhost:3306/'+argv[3])
-    conc = engine.connect()
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    Base = declarative_base()
+    conc = MySQLdb.connect("localhost", argv[1], argv[2], argv[3])
 
-    class states(Base):
-        '''Table defining states'''
-        __tablename__ = 'states'
+    curs = conc.cursor()
 
-        id = Column(Integer, autoincrement=True, primary_key=True)
-        name = Column(String(256))
+    curs.execute("SELECT * FROM states WHERE name='{}' ORDER BY id;".format(
+        argv[4]))
+    data = curs.fetchall()
 
-    for id, name in session.query(states.id, states.name).\
-        filter(states.name ==
-               "{}".format(argv[4])).\
-            order_by(states.id):
-        print("({}, '{}')".format(id, name))
+    for item in data:
+        print(item)
+
+    curs.close()
+    conc.close()
