da0902507b1a772ed0342931e07bb5b780091201
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 06:32:11 +0000

    Unpacks the two tables in a for loop and prints the contents

---
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa,
+in ascending order by state id'''
+
+
+import sqlalchemy
+import MySQLdb
+from model_state import *
+from sqlalchemy import MetaData
+from sqlalchemy import create_engine
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from sys import argv
+engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                       '@localhost:3306/'+argv[3])
+conc = engine.connect()
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
+class City(Base):
+    '''Table defining cities'''
+    __tablename__ = 'cities'
+
+    id = Column(Integer, autoincrement=True, primary_key=True)
+    name = Column(String(128), nullable=False)
+    state_id = Column(Integer, ForeignKey('State.id'))
