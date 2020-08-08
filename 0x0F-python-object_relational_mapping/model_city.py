371f696babc72184a0a5b75e7220013670446795
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 06:57:30 +0000

    No longer runs on import

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -3,27 +3,28 @@
 in ascending order by state id'''
 
 
-import sqlalchemy
-import MySQLdb
-from model_state import *
-from sqlalchemy import MetaData
-from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
-engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
-                       '@localhost:3306/'+argv[3])
-conc = engine.connect()
-Session = sessionmaker(bind=engine)
-session = Session()
-Base = declarative_base()
+if __name__ == "__main__":
+    import sqlalchemy
+    import MySQLdb
+    from model_state import *
+    from sqlalchemy import MetaData
+    from sqlalchemy import create_engine
+    from sqlalchemy import Column, Integer, String, ForeignKey
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                           '@localhost:3306/'+argv[3])
+    conc = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    Base = declarative_base()
 
 
-class City(Base):
-    '''Table defining cities'''
-    __tablename__ = 'cities'
+    class City(Base):
+        '''Table defining cities'''
+        __tablename__ = 'cities'
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
-    name = Column(String(128), nullable=False)
-    state_id = Column(Integer, ForeignKey('State.id'))
+        id = Column(Integer, autoincrement=True, primary_key=True)
+        name = Column(String(128), nullable=False)
+        state_id = Column(Integer, ForeignKey('State.id'))
