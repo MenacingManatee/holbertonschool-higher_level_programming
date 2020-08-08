d0f27f6c3d79fcf703149b50a6f2580563b42d17
Brett Davis <brett.p.davis101@gmail.com>
2020-08-08 01:00:00 +0000

    This could have been done with a filter, but this was easier

---
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa,
+in ascending order by state id'''
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                           '@localhost:3306/'+argv[3])
+    conc = engine.connect()
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    for id, name in session.query(State.id, State.name).order_by(
+            State.id):
+        if 'a' in name:
+            print("{}: {}".format(id, name))
