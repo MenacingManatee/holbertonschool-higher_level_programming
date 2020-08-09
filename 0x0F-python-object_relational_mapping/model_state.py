517448c6743732e8275e26ade9591ed3379e070c
Brett Davis <brett.p.davis101@gmail.com>
2020-08-09 23:36:54 +0000

    Updates model state with extra table constraints

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -23,5 +23,6 @@ class State(Base):
     '''Table defining states'''
     __tablename__ = 'states'
 
-    id = Column(Integer, autoincrement=True, primary_key=True)
+    id = Column(Integer, autoincrement=True, unique=True, nullable=False,
+                primary_key=True)
     name = Column(String(128), nullable=False)
