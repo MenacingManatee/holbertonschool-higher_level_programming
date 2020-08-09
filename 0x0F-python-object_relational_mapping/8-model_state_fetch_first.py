dba7697609700640b45aca8c167866814f7ee3db
Brett Davis <brett.p.davis101@gmail.com>
2020-08-09 23:53:39 +0000

    Now prints 'Nothing' if table is empty

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -9,6 +9,7 @@ if __name__ == "__main__":
     from sqlalchemy import create_engine
     from sqlalchemy.orm import sessionmaker
     from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy.orm.exc import NoResultFound
     from sys import argv
     engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
                            '@localhost:3306/'+argv[3])
@@ -23,7 +24,7 @@ if __name__ == "__main__":
         if count == 0:
             res = "{}: {}".format(id, name)
             count += 1
-    if res == ": ":
-        print("Nothing")
-    else:
+    if res != "":
         print(res)
+    else:
+        print("Nothing")
