8decc72b793a6617b67e926d717b214ed227c6fe
Brett Davis <brett.p.davis101@gmail.com>
2020-08-09 23:40:26 +0000

    removes unused imports and functions

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -3,19 +3,9 @@
 in ascending order by state id'''
 
 
-import sqlalchemy
-import MySQLdb
-from sqlalchemy import MetaData
-from sqlalchemy import create_engine
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from sys import argv
-engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
-                       '@localhost:3306/'+argv[3])
-conc = engine.connect()
-Session = sessionmaker(bind=engine)
-session = Session()
+
 Base = declarative_base()
 
 
