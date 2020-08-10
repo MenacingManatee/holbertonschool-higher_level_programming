aab13d1434fcc39772dde3235cc4b64f9c71e200
Brett Davis <brett.p.davis101@gmail.com>
2020-08-10 04:20:10 +0000

    Same as 101, but flipped for cities

---
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''lists all states from the database hbtn_0e_0_usa,
+in ascending order by state id'''
+
+
+if __name__ == "__main__":
+    import sqlalchemy
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy.ext.declarative import declarative_base
+    from sqlalchemy import MetaData
+    from sys import argv
+    engine = create_engine('mysql+mysqldb://'+argv[1]+':'+argv[2] +
+                           '@localhost:3306/'+argv[3])
+    metadata = MetaData()
+    conc = engine.connect()
+    Base.metadata.create_all(bind=engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    unf_res = session.query(City).order_by(City.id)
+
+    tmp = None
+    for city in unf_res:
+        print("{}: {} -> {}".format(city.id, city.name, city.state.name))
