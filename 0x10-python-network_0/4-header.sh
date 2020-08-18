cc92d930babb30b01692a1dc65a8cc3a54b45a9c
Brett Davis <brett.p.davis101@gmail.com>
2020-08-18 10:46:12 +0000

    Shellcheck fixes

--- 0x10-python-network_0/4-header.sh
+++ 0x10-python-network_0/4-header.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 # takes in a URL as an argument, sends a GET request to the URL,
 # and displays the body of the response
-curl -H "X-HolbertonSchool-User-Id: 98" $1
+curl -H "X-HolbertonSchool-User-Id: 98" "$1"
--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,2 +1,4 @@
 #!/usr/bin/env bash
-
+# sends a POST request an displays response
+curl --data "email=hr@holbertonschool.com&subject=I will always be here \
+for PLD" "$1"
