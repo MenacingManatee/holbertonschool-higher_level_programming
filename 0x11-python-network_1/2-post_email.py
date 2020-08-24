b436a0e6a38a2561441505e624cc5cecf050b208
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 21:39:55 +0000

    It's much easier to see the output when I'm in the right container

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''sends a POST request to the passed URL with the email as a parameter'''
+
+
+import urllib.request, urllib.parse
+from sys import argv
+
+
+data = urllib.parse.urlencode({"email": argv[2]}).encode()
+with urllib.request.urlopen(argv[1], data=data) as u:
+    print(u.read().decode('utf-8'))
+    u.close()
