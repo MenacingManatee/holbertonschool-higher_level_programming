2915832c2988c6c9b8f09cb175fea194756f0b6c
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 23:10:53 +0000

    Shortened comment

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-'''takes in a URL, sends a request to the URL and displays the body \
-of the response'''
+'''takes in a URL, sends a request to the URL and displays the body'''
 
 
 if __name__ == "__main__":
