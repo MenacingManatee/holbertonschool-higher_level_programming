d02ab2f9a7590dc9ef580e24e660c2c72101d333
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 08:44:55 +0000

    Uses getheader to avoid searching through all the headers

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status using urllib'''
+
+
+import urllib.request
+
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as u:
+    print(u.getheader('X-Request-Id'))
+    u.close()
