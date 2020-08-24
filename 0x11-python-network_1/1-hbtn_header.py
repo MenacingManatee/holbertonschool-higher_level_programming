8a5ae1cfd29b14d782671e49da961227b3cdb3ad
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 21:44:47 +0000

    Code no longer executes on import

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 '''fetches https://intranet.hbtn.io/status using urllib'''
 
+if __name__ == "__main__":
+    from urllib import request
+    import sys
 
-from urllib import request
-import sys
-
-with request.urlopen(sys.argv[1]) as u:
-    print(u.getheader('X-Request-Id'))
-    u.close()
+    with request.urlopen(sys.argv[1]) as u:
+        print(u.getheader('X-Request-Id'))
+        u.close()
