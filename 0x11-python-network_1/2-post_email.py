15cc1fa3d8d6702c16dbf1ba602197c3b5ecfea6
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 21:47:05 +0000

    Code no longer executes on import, fixed pep8 errors

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -2,11 +2,11 @@
 '''sends a POST request to the passed URL with the email as a parameter'''
 
 
-import urllib.request, urllib.parse
-from sys import argv
+if __name__ == "__main__":
+    from urllib import request, parse
+    from sys import argv
 
-
-data = urllib.parse.urlencode({"email": argv[2]}).encode()
-with urllib.request.urlopen(argv[1], data=data) as u:
-    print(u.read().decode('utf-8'))
-    u.close()
+    data = parse.urlencode({"email": argv[2]}).encode()
+    with request.urlopen(argv[1], data=data) as u:
+        print(u.read().decode('utf-8'))
+        u.close()
