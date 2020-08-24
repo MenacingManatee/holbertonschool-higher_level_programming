6bcbcb9dd4cb995c6fd4ff9c45795f971f092c75
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 23:23:32 +0000

    Gets the headers, and gets the specified header from the dict

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status using requests module'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    req = requests.head(argv[1])
+    print(req.headers.get('X-Request-Id'))
