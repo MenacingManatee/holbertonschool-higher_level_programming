06829b69595382c4b04894197381150c07aeb42a
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 08:41:41 +0000

    reads the url and prints info about it

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status using urllib'''
+
+
+import urllib.request
+
+
+with urllib.request.urlopen("https://intranet.hbtn.io/status") as u:
+    tmp = u.read()
+    print("Body response:")
+    print("\t- type: {}".format(type(tmp)))
+    print("\t- content: {}".format(tmp))
+    print("\t- utf8 content: {}".format(tmp.decode('utf-8')))
+    u.close()
