f7b7f9e8f82464b8d5ed2052a77d174a2bcf872d
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 23:17:34 +0000

    fetches https://intranet.hbtn.io/status and displays the body response

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status using requests module'''
+
+
+if __name__ == "__main__":
+    import requests
+
+    body = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:\n\t-type: {}\n\t- content: {}".format(
+        type(body.text), body.text))
