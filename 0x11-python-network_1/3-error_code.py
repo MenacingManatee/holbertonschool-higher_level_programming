92e896ef3e2595ca78660dc971207ea106c1d210
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 23:01:20 +0000

    Uses a try-except block to get the error code on fail

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+'''takes in a URL, sends a request to the URL and displays the body 
+of the response'''
+
+
+if __name__ == "__main__":
+    from urllib import request, error
+    from sys import argv
+
+    try:
+        with request.urlopen(argv[1]) as u:
+            print(u.read().decode('utf8'))
+    except error.HTTPError as e:
+        print("Error Code:", e.getcode())
