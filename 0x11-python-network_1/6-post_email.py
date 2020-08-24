417a76fa108f4e7da7e1caf95f6ae21ce97046be
Brett Davis <brett.p.davis101@gmail.com>
2020-08-24 23:57:45 +0000

    Posts the variable then displays the body

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+'''fetches https://intranet.hbtn.io/status using requests module'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    req = requests.post(argv[1], data = {"email": argv[2]})
+    print(req.text)
