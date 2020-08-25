410cb57983a4ded4ea3aa427c6b00a97c6c85fee
Brett Davis <brett.p.davis101@gmail.com>
2020-08-25 02:24:35 +0000

    Minor concern using json might cause errors, needs testing

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+'''takes your Github credentials (username and password)
+and uses the Github API to display your id'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = 'https://api.github.com/user'
+    req = requests.get(url, auth=(argv[1], argv[2]))
+    print(req.json().get('id'))
