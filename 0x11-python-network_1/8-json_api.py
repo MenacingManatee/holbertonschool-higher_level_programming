de99e3184fbe854166a0a22e01ee2fdc1880a79c
Brett Davis <brett.p.davis101@gmail.com>
2020-08-25 02:03:04 +0000

    TODO: Test invalid json

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,1 +1,25 @@
 #!/usr/bin/python3
+'''takes in a letter and sends a POST request to
+http://0.0.0.0:5000/search_user with the letter as a parameter.'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    if len(argv) < 2:
+        js = ""
+    else:
+        js = "".join(argv[1:])
+    print(js)
+    url = 'http://0.0.0.0:5000/search_user'
+    req = requests.post(url, data={"q": js})
+    try:
+        js2 = req.json()
+    except Exception:
+        print("Not a valid JSON")
+        exit
+    if js2 == {}:
+        print("No result")
+    else:
+        print("[{}] {}".format(js2.get('id'), js2.get('name')))
