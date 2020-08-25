b879e20a812f9e5d2324d359d044e0ff6d8530b0
Brett Davis <brett.p.davis101@gmail.com>
2020-08-25 00:03:30 +0000

    Displays code on error, body otherwise

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+'''takes in a URL, sends a request to the URL and displays the body'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    req = requests.get(argv[1])
+    if req.status_code >= 400:
+        print("Error code: {}".format(req.status_code))
+    else:
+        print(req.text)
