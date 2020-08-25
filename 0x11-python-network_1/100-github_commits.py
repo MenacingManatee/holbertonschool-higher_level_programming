4f351f2ad1b1d76f959ee1a9d2c44cc1585ad1e8
Brett Davis <brett.p.davis101@gmail.com>
2020-08-25 02:56:16 +0000

    Running a quick errand. Formatting appears correct, need to cut to only last 10 commits

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+'''takes your Github credentials (username and password)
+and uses the Github API to display your id'''
+
+
+if __name__ == "__main__":
+    import requests
+    from sys import argv
+
+    url = "".join(['https://api.github.com/repos/', argv[2], '/',
+                   argv[1], '/commits'])
+    req = requests.get(url)
+    for item in req.json():
+#        print(item, '\n\n\n')
+        print(item.get('sha'), item.get('commit').get('author').
+              get('name'))
