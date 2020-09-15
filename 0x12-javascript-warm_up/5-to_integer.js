b43441d3b45e557d70e8b70a6b72ee99ed5b5e3d
Brett Davis <brett.p.davis101@gmail.com>
2020-09-15 22:17:05 +0000

    Semistandard fixes

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,10 +1,11 @@
 #!/usr/bin/node
 
 const num = Math.trunc(Number(process.argv[2]));
+const num2 = num;
 if (process.argv[2] === undefined) {
   console.log('Not a number');
-} else if (num != num) {
+} else if (num !== num2) {
   console.log('Not a number');
 } else {
-  console.log('My number:', num)
+  console.log('My number:', num);
 }
