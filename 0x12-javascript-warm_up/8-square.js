efa27fdd236597afca5fc1607d1ac75ca82a27ae
Brett Davis <brett.p.davis101@gmail.com>
2020-09-15 23:52:58 +0000

    Semistandard fixes

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const num1 = Math.trunc(Number(process.argv[2]));
 const num2 = num1;
 if (num1 !== num2) {
-    console.log('Missing size');
+  console.log('Missing size');
 } else {
   let i;
   let j;
@@ -12,7 +12,7 @@ if (num1 !== num2) {
     for (j = 0; j < num1; j++) {
       str = str + '#';
     }
-    console.log(str)
-    str = ''
+    console.log(str);
+    str = '';
   }
 }
