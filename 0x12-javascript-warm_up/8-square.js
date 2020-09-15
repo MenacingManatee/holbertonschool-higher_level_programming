0a409d9561db0f32ef54dc887f2af2e3362840ff
Brett Davis <brett.p.davis101@gmail.com>
2020-09-15 23:52:36 +0000

    Uses a double for loop and concatenates string to build square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+const num1 = Math.trunc(Number(process.argv[2]));
+const num2 = num1;
+if (num1 !== num2) {
+    console.log('Missing size');
+} else {
+  let i;
+  let j;
+  let str = '';
+  for (i = 0; i < num1; i++) {
+    for (j = 0; j < num1; j++) {
+      str = str + '#';
+    }
+    console.log(str)
+    str = ''
+  }
+}
