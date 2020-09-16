951835f6e951bc3b95228799fce45869c584c0ab
Brett Davis <brett.p.davis101@gmail.com>
2020-09-16 01:26:52 +0000

    Potential error if second biggest is negative

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+if (process.argv.length <= 3) {
+  console.log('0');
+} else {
+  let arr = [Math.trunc(Number(process.argv[2])), 0]
+  let i;
+  for (i = 3; i < process.argv.length; i++) {
+    if (Math.trunc(Number(process.argv[i])) >= arr[0]) {
+      arr[1] = arr[0]
+      arr[0] = Math.trunc(Number(process.argv[i]))
+    } else if (Math.trunc(Number(process.argv[i])) >= arr[1]) {
+      arr[1] = Math.trunc(Number(process.argv[i]))
+    }
+  }
+  console.log(arr[1])
+}
