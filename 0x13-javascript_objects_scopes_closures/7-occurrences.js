0e6e55e169b4219b0aad9aafe59d84bfe9c0191f
Brett Davis <brett.p.davis101@gmail.com>
2020-09-21 21:40:52 +0000

    Counts the number of occurrences of an element in a list

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,2 +1,14 @@
 #!/usr/bin/node
 
+exports.nbOccurences = function (list, searchElement) {
+  if (list === undefined) {
+    return 0;
+  }
+  let count = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (list[i] === searchElement) {
+      count += 1;
+    }
+  }
+  return count;
+};
