07effeef685d30cd2e3251030ae28b655e725e5b
Brett Davis <brett.p.davis101@gmail.com>
2020-09-21 11:22:43 +0000

    Adds printChar to square

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,2 +1,21 @@
 #!/usr/bin/node
 
+const Rectangle = require('./4-rectangle');
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.width; i++) {
+      let p = '';
+      for (let j = 0; j < this.width; j++) {
+        p = p + c;
+      }
+      console.log(p);
+    }
+  }
+};
