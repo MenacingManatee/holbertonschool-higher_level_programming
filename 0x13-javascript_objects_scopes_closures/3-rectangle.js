524480bf4d1a615750dde49bc2b0931c359975c2
Brett Davis <brett.p.davis101@gmail.com>
2020-09-21 10:14:43 +0000

    Added print method to class

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
 
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (x) {
+    for (let i = 0; i < this.height; i++) {
+      let p = '';
+      for (let j = 0; j < this.width; j++) {
+        p = p + 'X';
+      }
+      console.log(p);
+    }
+  }
+};
