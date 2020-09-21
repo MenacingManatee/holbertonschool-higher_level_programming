8c382c7c40cdbf13e75e2fc282513712a08d87b5
Brett Davis <brett.p.davis101@gmail.com>
2020-09-21 23:24:30 +0000

    Reverses the keys and values of an object

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const dict = require('./101-data').dict;
+const resDict = {};
+console.log(dict);
+for (const [key, val] of Object.entries(dict)) {
+  let tmp;
+  if (resDict[val] !== undefined) {
+    tmp = resDict[val];
+    tmp.push(key);
+  } else {
+    tmp = [key];
+  }
+  const tmp2 = { [val]: tmp };
+  Object.assign(resDict, tmp2);
+}
+console.log(resDict);
