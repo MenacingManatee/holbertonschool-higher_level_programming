095449019abcce511000ad2b70802894bb4f8bb4
Brett Davis <brett.p.davis101@gmail.com>
2020-09-21 10:17:43 +0000

    Adds the rotate and double functions to rectangle class

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,2 +1,31 @@
 #!/usr/bin/node
 
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print (x) {
+    for (let i = 0; i < this.height; i++) {
+      let p = '';
+      for (let j = 0; j < this.width; j++) {
+        p = p + 'X';
+      }
+      console.log(p);
+    }
+  }
+
+  rotate () {
+    const tmp = this.width;
+    this.width = this.height;
+    this.height = tmp;
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+};
