18c6443bfb647f5ee884cc67419ab9d3fd6f9cc4
Brett Davis <brett.p.davis101@gmail.com>
2020-09-22 23:33:27 +0000

    Semistandard fixes

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -5,13 +5,12 @@ const request = require('request');
 request(process.argv[2], function (err, res, body) {
   if (err) { console.log(err); }
   const jsn = JSON.parse(body);
-  let resObj = {};
-  for (obj in jsn) {
+  const resObj = {};
+  for (const obj in jsn) {
     if (jsn[obj].completed) {
       if (resObj[jsn[obj].userId]) {
         resObj[jsn[obj].userId] += 1;
-      }
-      else {
+      } else {
         resObj[jsn[obj].userId] = 1;
       }
     }
