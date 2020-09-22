8da819883fa5d630b96af6af4e903c08490cedef
Brett Davis <brett.p.davis101@gmail.com>
2020-09-22 13:32:31 +0000

    Added an extra 'for' loop because 'in' keyword wasn't working as expected

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,2 +1,17 @@
 #!/usr/bin/node
 
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) { console.log(err); }
+  const jsn = JSON.parse(body);
+  let count = 0;
+  for (let i = 0; i < jsn.results.length; i++) {
+    for (let j = 0; j < jsn.results[i].characters.length; j++) {
+      if (jsn.results[i].characters[j] === 'https://swapi-api.hbtn.io/api/people/18/') {
+        count += 1;
+      }
+    }
+  }
+  console.log(count);
+});
