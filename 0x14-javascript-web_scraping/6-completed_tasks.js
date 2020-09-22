92784d98ce6880e0e1502aa9b721784421739503
Brett Davis <brett.p.davis101@gmail.com>
2020-09-22 23:32:14 +0000

    obj was giving index instead of objects, I keep forgetting python syntax doesn't always quite work

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,2 +1,20 @@
 #!/usr/bin/node
 
+const request = require('request');
+
+request(process.argv[2], function (err, res, body) {
+  if (err) { console.log(err); }
+  const jsn = JSON.parse(body);
+  let resObj = {};
+  for (obj in jsn) {
+    if (jsn[obj].completed) {
+      if (resObj[jsn[obj].userId]) {
+        resObj[jsn[obj].userId] += 1;
+      }
+      else {
+        resObj[jsn[obj].userId] = 1;
+      }
+    }
+  }
+  console.log(resObj);
+});
