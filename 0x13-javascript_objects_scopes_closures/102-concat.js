dd1ad7b90036fca2da541f72b46c2084533d5219
Brett Davis <brett.p.davis101@gmail.com>
2020-09-22 00:03:10 +0000

    Data was not being saved, so just writing and appending instead of doing the writing all at once

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,2 +1,28 @@
 #!/usr/bin/node
 
+const fs = require('fs')
+
+let file1;
+let file2;
+let tmp = fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+  if (err) {
+    file1 = '';
+  }
+  else {
+    file1 = data;
+  }
+  fs.writeFile(process.argv[4], file1, function (err) {
+    if (err) throw err;
+    });
+});
+fs.readFile(process.argv[3], 'utf-8', (err, data) => {
+  if (err) {
+    file2 = '';
+  }
+  else {
+    file2 = data;
+  }
+  fs.appendFile(process.argv[4], file2, function (err) {
+    if (err) throw err;
+  });
+});
