be7d3224d02bec3c659738b7a7b43113c01b0928
Brett Davis <brett.p.davis101@gmail.com>
2020-09-23 00:01:59 +0000

    semistandard fix

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -6,12 +6,10 @@ request('https://swapi-api.hbtn.io/api/films/' + process.argv[2],
   function (err, res, body) {
     if (err) { console.log(err); }
     const jsn = JSON.parse(body);
-    const chars = jsn.characters.sort(function sortChars(a, b) {
+    const chars = jsn.characters.sort(function sortChars (a, b) {
       const num1 = parseInt(a.slice(37, 39));
       const num2 = parseInt(b.slice(37, 39));
-      if (num1 > num2) { return 1; }
-      else if (num2 > num1 ) { return -1; }
-      else { return 0; }
+      if (num1 > num2) { return 1; } else if (num2 > num1) { return -1; } else { return 0; }
     });
     for (let i = 0; i < chars.length; i++) {
       request(chars[i], function (err, res, body) {
